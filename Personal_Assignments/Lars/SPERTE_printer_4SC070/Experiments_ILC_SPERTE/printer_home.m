5e0975f727a3b610f2282fd613b61dfc1392380c
mariekeschippers02 <marieke.schippers02@gmail.com>
2026-06-08 12:30:17 +0200

    clean branch

--- Personal_Assignments/Lars/SPERTE_printer_4SC070/Experiments_ILC_SPERTE/printer_home.m
+++
@@ -1,5 +0,0 @@
-function printer_home(Index_home)
-simulink_set_constant(Index_home, 1);
-pause(0.2);
-simulink_set_constant(Index_home, 0);
-end
--- Personal_Assignments/Lars/SPERTE_printer_4SC070/Experiments_ILC_SPERTE/simulink_set_constant.m
+++
@@ -1,8 +0,0 @@
-function simulink_set_constant(block, signal)  
-if length(signal) > 1
-set_param(block,"Value",strcat("[",num2str(signal(:)'),"]'"));
-else
-    
-set_param(block,"Value",num2str(signal));
-
-end
